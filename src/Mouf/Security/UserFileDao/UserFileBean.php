7feb530dff4cae855641aaf7db4dd67933e983b9
David Négrier <david@davidtcm>
2012-09-09 11:28:19 +0200

    Initial import

---
+++ src/Mouf/Security/UserFileDao/UserFileBean.php
@@ -0,0 +1,59 @@
+<?php
+namespace Mouf\Security\UserFileDao;
+
+/**
+ * This represents a User the way it is stored by the UserFileDao.
+ * Basically, it has a login, password (encoded in sha1), and some options.
+ * 
+ * @author David
+ *
+ */
+class UserFileBean implements UserInterface {
+	
+	public $login;
+	public $password;
+	public $options;
+	
+	public function __construct($login = null, $password = null, $options = null) {
+		$this->login = $login;
+		$this->password = $password;
+		$this->options = $options;
+	}
+	
+	/**
+	 * Returns the ID for the current user.
+	 *
+	 * @return string
+	 */
+	public function getId() {
+		return $this->login;
+	}
+	
+	/**
+	 * Returns the login for the current user.
+	 *
+	 * @return string
+	 */
+	public function getLogin() {
+		return $this->login;
+	}
+	
+	/**
+	 * Returns the password for the current user.
+	 *
+	 * @return string
+	 */
+	public function getEncodedPassword() {
+		return $this->password;
+	}
+
+	/**
+	 * Returns the options for the current user.
+	 *
+	 * @return string
+	 */
+	public function getOptions() {
+		return $this->options;
+	}
+	
+}
---
+++ src/Mouf/Security/UserFileDao/UserFileDaoException.php
@@ -0,0 +1,10 @@
+<?php
+namespace Mouf\Security\UserFileDao;
+
+/**
+ * Thrown when an error is detected in the UserFileDao.
+ */
+class UserFileDaoException extends Exception {
+
+}
+?>
